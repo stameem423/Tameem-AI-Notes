56b117e77d0cdc759882f7b3f57d8fd7ab8033e2
stameem423 <stameem423@gmail.com>
2026-05-06 22:40:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ else:
 
 if api_key:
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-3-flash')
+    model = genai.GenerativeModel('gemini-3-flash-preview')
 else:
     st.error("⚠️ Developer Note: API Key missing in Streamlit Secrets!")
 
